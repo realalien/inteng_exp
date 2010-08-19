69e6ca75fc1880bf7304712e778a2e9727142a6d
unknown <ZhuJiaCheng@.SpicyNet.local>
2010-08-19 16:38:07 +0800

    init

---
+++ strategy_pattern_example.rb
@@ -0,0 +1,33 @@
+class Context
+  def initialize(strategy)
+    self.class.send :include, strategy
+  end
+end
+ 
+module StrategyA
+  def execute
+     puts 'Doing the task the normal way'
+  end
+end
+ 
+module StrategyB
+  def execute
+     puts 'Doing the task alternatively'
+  end
+end
+ 
+module StrategyC
+  def execute
+     puts 'Doing the task even more alternatively'
+  end
+end
+ 
+a = Context.new(StrategyA)
+a.execute #=> Doing the task the normal way
+ 
+b = Context.new(StrategyB)
+b.execute #=> Doing the task alternatively
+ 
+c = Context.new(StrategyC)
+c.execute #=> Doing the task even more alternatively
+
